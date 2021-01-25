219f6507b4ef185b170bd2e39cef19cb29c041c8
ririko <ririmohu25@gmail.com>
2021-01-25 23:17:21 +0900

    First test add

--- _test.js
+++ _test.js
@@ -1,5 +1,8 @@
-// Feature('');
+/* eslint-disable */
 
-// Scenario('test something', ({ I }) => {
+Feature("blitz初期ページテストをテストしてみる！") // このテスト全体の名前
 
-// });
+Scenario("blitz初期ページに対象のテキストが表示されているかどうかをテスト！", ({ I }) => {
+  I.amOnPage("http://localhost:3000/")
+  I.see("Congrats!")
+})
--- codecept.conf.js
+++ codecept.conf.js
@@ -9,7 +9,7 @@ exports.config = {
   output: "./output",
   helpers: {
     Puppeteer: {
-      url: "http://localhost",
+      url: "http://localhost:3000/",
       show: true,
       windowSize: "1200x900",
     },
